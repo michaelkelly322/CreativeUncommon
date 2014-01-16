bd804f157984bd1565436694778fc113db14a903
Mike Kelly <michaelkelly322@gmail.com>
2014-01-16 00:47:16 -0500

    Setup for TDD created initial User MVC
    Installed and prepared rspec, used default generator
    to create controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TreeHouse::Application.routes.draw do
+  get "users/new"
   get "site/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
