7f96405fe7638d44a86f4e02cf3a5e7a685fdbf8
Mike Kelly <michaelkelly322@gmail.com>
2014-02-02 02:49:09 -0500

    Expanded authentication tests
    signin with invalid info is tested
    signin with valid is pending

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -21,4 +21,21 @@ describe "Authentication" do
       it { should have_selector('a', value: "Signup")}
     end
   end
+  
+  describe "signin" do
+    before { visit signin_path }
+    
+    describe "with invalid information" do
+      before { click_button "Sign in"}
+      
+      it { should have_title('Sign In')}
+      it { should have_selector('div.alert.alert-error')}
+    end
+    
+    describe "with valid information" do
+      it "should create a session"
+      it "should change page with user context"
+      it "should update backend for login"
+    end
+  end
 end
