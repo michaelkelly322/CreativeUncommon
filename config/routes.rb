eeda457f5e20e2ce92ce156c1d9d1c68383bd4ad
Mike Kelly <michaelkelly322@gmail.com>
2014-02-02 02:31:10 -0500

    route file cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 TreeHouse::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  get "users/new"
-  get "site/home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
