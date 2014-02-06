d211f9bd92ef5d58ed4e768ad2c4d31132fa14ad
Mike Kelly <michaelkelly322@gmail.com>
2014-02-06 17:30:04 -0500

    Added sign_in sing_in? and current_user to session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,25 @@
 module SessionsHelper
+  def sign_in(user)
+    session_key = User.new_session_key
+    session[:session_key] = session_key
+    user.update_attribute(:session_key, User.encrypt(session_key))
+    self.current_user = user
+  end
+  
+  def current_user=(user)
+    @current_user = user
+  end
+  
+  def current_user
+    if @current_user.nil?
+      session_key = User.encrypt(session[:session_key])
+      @current_user = User.find_by(session_key: session_key)
+    else
+      @current_user
+    end
+  end
+  
+  def signed_in?
+    !current_user.nil?
+  end
 end
