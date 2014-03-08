379e0cf4a8c21a032562f3aa2ff5367cc9945f32
Michael Kelly <michaelkelly322@gmail.com>
2014-03-08 01:47:00 -0500

    Added validations to work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,5 +2,13 @@ class Work < ActiveRecord::Base
   belongs_to :user
   
   validates :user_id, presence: true
-  validates :title, presence: true
+  validates :title,
+              presence: true,
+              length: { maximum: 100 }
+              
+  validates :blurb,
+              presence: true,
+              length: { maximum: 250 }
+              
+  validates :body, presence: true
 end
