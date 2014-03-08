58e7b503bf8d77aa87267a10607bd533c2d861a6
Michael Kelly <michaelkelly322@gmail.com>
2014-03-08 01:22:48 -0500

    Precompiled assets agained, accidentally deleted

--- config/initializers/genre_types.rb
+++ config/initializers/genre_types.rb
@@ -1,1 +1,1 @@
-GENRE_CONFIG = YAML.load_file(Rails.root.join('config', 'genres.yml') unless defined?(GENRE_CONFIG)
+GENRE_CONFIG = YAML.load_file(Rails.root.join('config', 'genres.yml')) unless defined?(GENRE_CONFIG)
