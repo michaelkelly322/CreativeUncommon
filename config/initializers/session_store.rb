be651191ad037925659fe4d44afc7c2b7bac5358
Michael Kelly <michaelkelly322@gmail.com>
2014-03-16 02:20:49 -0400

    removed all creative uncommons references

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TreeHouse::Application.config.session_store :cookie_store, key: '_CreativeUncommons_session'
+TreeHouse::Application.config.session_store :cookie_store, key: '_fuinneamh_session'
