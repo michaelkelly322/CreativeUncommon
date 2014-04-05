79941ca0a6f9c4d96e9db185222b01c47bb83462
Michael Kelly <michaelkelly322@gmail.com>
2014-04-05 03:56:55 -0400

    Initial dev release of portfolio

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -6,4 +6,78 @@ module SiteHelper
     
     out.html_safe
   end
+  
+  def fill_stories(path)
+    card_path = Rails.root.join('app', 'assets', path)
+    content = Hash.new
+    
+    Dir.glob(card_path.join('*.txt')) do |file|
+      logger.debug "#{file}"
+      
+      file_name = file.split('/').last
+      
+      story_name_raw = file_name.split('.').first
+      story_name = story_name_raw.split.map(&:capitalize).join(' ')
+      
+      File.open(file) do |text|
+        content[story_name] = text.read
+      end
+    end
+    
+    return content
+  end
+  
+  def fill_images(path)
+    image_path = Rails.root.join('app', 'assets', 'images', path)
+    content = Hash.new
+    
+    Dir.glob(image_path.join('*')) do |file|
+      file_name = file.split('/').last
+      
+      image_name_raw = file_name.split('.').first
+      image_name = image_name_raw.split.map(&:capitalize).join(' ')
+      
+      content[image_name] = file
+    end
+    
+    return content
+  end
+  
+  def fill_cover_photos(path)
+    photo_path = Rails.root.join('app', 'assets', 'images', path)
+    content = Hash.new
+    
+    Dir.glob(photo_path.join('*_cover.JPG')) do |file|
+      file_name = file.split('/').last
+      
+      photo_name_raw = file_name.split('.').first
+      photo_name_raw.slice!('_cover')
+      photo_name_raw.slice!('e_')
+      photo_name = photo_name_raw.split.map(&:capitalize).join(' ')
+      
+      
+      content[photo_name] = file
+    end
+    
+    return content
+  end
+  
+  def fill_photos(path)
+    photo_path = Rails.root.join('app', 'assets', 'images', path)
+    content = Hash.new
+    
+    Dir.glob(photo_path.join('*.JPG')) do |file|
+      file_name = file.split('/').last
+      
+      photo_name_raw = file_name.split('.').first
+      photo_name_raw.slice!('_cover')
+      photo_name_raw.slice!('e_')
+      photo_name = photo_name_raw.split.map(&:capitalize).join(' ')
+      
+      
+      content[photo_name] = file
+    end
+    
+    return content
+  end
 end
